8322f5918082d4837147d307fdfcc9caece21bc1
devnabibia <nangendodennis@gmail.com>
2024-03-18 21:45:43 +0300

    dev

--- src/components/ErrorElement/index.tsx
+++ src/components/ErrorElement/index.tsx
@@ -8,7 +8,7 @@ const ErrorElement = () => {
 
   return (
     <div className="w-screen h-screen flex items-center justify-center flex-col gap-4">
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Sorry, an unexpected error has occurred. Try again</p>
       <p>
         <i className="text-lg font-bold">{error.statusText || error.message}</i>
       </p>
